7552d92b0a64dcc95fd18f171214e0ab0f7e1152
DevJogger <158173870+DevJogger@users.noreply.github.com>
2025-07-24 22:05:00 -0700

    fix(load templates): close dialog after clicking any buttons in the dialog

--- app/page.tsx
+++ app/page.tsx
@@ -23,6 +23,7 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
+  DialogClose,
 } from '@/components/ui/dialog'
 import { Trash } from 'lucide-react'
 import { cn } from '@/lib/utils'
@@ -139,28 +140,32 @@ export default function Home() {
               <div className='mt-4 flex flex-col gap-2'>
                 {DUMMY_DATA.map((template) => (
                   <div className='flex items-center gap-2' key={template.uuid}>
-                    <Button
-                      className='flex-1 cursor-pointer'
-                      variant='outline'
-                      onClick={() => {
-                        setSurveyData(template.data)
-                        setCurrentTemplateId(template.uuid)
-                      }}
-                    >
-                      {template.uuid}
-                    </Button>
-                    <Button
-                      className='cursor-pointer'
-                      onClick={() => {
-                        const url = `${window.location.origin}/${template.uuid}`
-                        // copy the template uuid to clipboard
-                        navigator.clipboard.writeText(url).then(() => {
-                          alert('Template URL copied to clipboard!')
-                        })
-                      }}
-                    >
-                      Share
-                    </Button>
+                    <DialogClose asChild>
+                      <Button
+                        className='flex-1 cursor-pointer'
+                        variant='outline'
+                        onClick={() => {
+                          setSurveyData(template.data)
+                          setCurrentTemplateId(template.uuid)
+                        }}
+                      >
+                        {template.uuid}
+                      </Button>
+                    </DialogClose>
+                    <DialogClose asChild>
+                      <Button
+                        className='cursor-pointer'
+                        onClick={() => {
+                          const url = `${window.location.origin}/${template.uuid}`
+                          // copy the template uuid to clipboard
+                          navigator.clipboard.writeText(url).then(() => {
+                            alert('Template URL copied to clipboard!')
+                          })
+                        }}
+                      >
+                        Share
+                      </Button>
+                    </DialogClose>
                   </div>
                 ))}
               </div>
